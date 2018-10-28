ae4d938c4e05086552b100d8c34b896976cc3170
squall1744 <squall1744@163.com>
2018-10-28 15:33:26 +0800

    first commit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Hello world</p>
+</template>
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
---
+++ src/pages/list/List.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>lsit</p>
+</template>
+
+<script>
+export default {
+  name: 'list'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
