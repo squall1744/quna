7b5b4a3c0ca53a80d6220bac428ebdf3013b2c58
squall1744 <squall1744@163.com>
2018-10-29 15:49:24 +0800

    add

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 
 </template>
@@ -11,12 +12,14 @@
 import homeHeader from "./components/Header";
 import homeSwiper from "./components/Swiper";
 import homeIcons from "./components/Icons";
+import homeRecommend from "./components/Recommend";
 export default {
   name: "home",
   components: {
     homeHeader,
     homeSwiper,
-    homeIcons
+    homeIcons,
+    homeRecommend
   }
 };
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "recommend"
+};
+</script>
+
+<style lang="stylus" scoped>
+.recommend-title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+</style>
+
